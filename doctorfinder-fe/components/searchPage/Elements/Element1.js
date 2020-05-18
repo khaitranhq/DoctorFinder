b2b238d26e1db35cb784119b7013070abb2bbc2f
lioaslan <arey.tieuleo.5.7@gmail.com>
2020-05-18 17:32:46 +0700

    implement first element

--- doctorfinder-fe/components/searchPage/Elements/Element1.js
+++ doctorfinder-fe/components/searchPage/Elements/Element1.js
@@ -1,44 +1,140 @@
 import React from "react";
-import { Grid, withStyles } from "@material-ui/core";
+import { Grid, withStyles, TextField, Button } from "@material-ui/core";
+import { Autocomplete } from "@material-ui/lab";
 
-const styles = theme => ({
+const styles = (theme) => ({
     img: {
-        position: "absolute",
         width: "100%",
-        height: theme.spacing(75)
+        height: theme.spacing(60),
     },
-    secondElement: {
-        height: theme.spacing(75),
-        zIndex: 1
+    form: {
+        width: "100%",
+        height: theme.spacing(60),
+        zIndex: 1,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        color: "#fff",
+        font: "",
     },
     bigTitle: {
-        color: "#000"
+        fontFamily: "Roboto",
+        fontSize: 50,
+        fontWeight: 800,
     },
     smallTitle: {
-        color: "#fff"
+        fontFamily: "Roboto",
+        fontSize: 20,
+        fontWeight: 800,
+    },
+    specialtyInput: {
+        background: "#fff",
+        borderRadius: 5,
+        width: 236,
+        height: 52,
+    },
+    wrapInput: {
+        marginTop: 30,
+        marginBottom: 6
+    },
+    btn: {
+        background: "#3163C1",
+        width: 100,
+        height: 40
     }
 });
 
-const Element1 = props => {
+const specialties = [{ title: "DFSDF", year: 1994 }];
+
+const Element1 = (props) => {
     const { classes } = props;
     return (
         <div className={classes.root}>
-            <Grid>
-                <img
-                    src="../../../static/images/download.png"
-                    className={classes.img}
-                />
-            </Grid>
-            <Grid container className={classes.secondElement}>
-                <Grid item>
-                    <h1 className={classes.bigTitle}>DOCTOR FINDER</h1>
+            <img
+                src="../../../static/images/download.png"
+                className={classes.img}
+            />
+            <div className={classes.form}>
+                <Grid
+                    container
+                    alignItems="center"
+                    justify="center"
+                    style={{ height: "100%" }}
+                    direction="column"
+                >
+                    <Grid item>
+                        <label className={classes.bigTitle}>
+                            Find The Best Doctor
+                        </label>
+                    </Grid>
+                    <Grid item>
+                        <label className={classes.smallTitle}>
+                            Find the best right fit doctor for you
+                        </label>
+                    </Grid>
+                    <Grid item container justify="center" spacing={2} className={classes.wrapInput}>
+                        <Grid item>
+                            <Autocomplete
+                                options={specialties.map(
+                                    (option) => option.title
+                                )}
+                                renderInput={(params) => (
+                                    <TextField
+                                        {...params}
+                                        label="Specialty"
+                                        margin="normal"
+                                        variant="outlined"
+                                        InputProps={{
+                                            className: classes.specialtyInput,
+                                        }}
+                                    />
+                                )}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Autocomplete
+                                options={specialties.map(
+                                    (option) => option.title
+                                )}
+                                renderInput={(params) => (
+                                    <TextField
+                                        {...params}
+                                        label="Specialty"
+                                        margin="normal"
+                                        variant="outlined"
+                                        InputProps={{
+                                            className: classes.specialtyInput,
+                                        }}
+                                    />
+                                )}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Autocomplete
+                                options={specialties.map(
+                                    (option) => option.title
+                                )}
+                                renderInput={(params) => (
+                                    <TextField
+                                        {...params}
+                                        label="Specialty"
+                                        margin="normal"
+                                        variant="outlined"
+                                        InputProps={{
+                                            className: classes.specialtyInput,
+                                        }}
+                                    />
+                                )}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Grid item>
+                        <Button variant="contained" color="primary" className={classes.btn}>
+                            Search
+                        </Button>
+                    </Grid>
                 </Grid>
-                <Grid item>
-                    <label className={classes.smallTitle}>
-                        Find the best right fit doctor for you
-                    </label>
-                </Grid>
-            </Grid>
+            </div>
         </div>
     );
 };
