4bb7bdf023e59745fee79bce10a5c707efda96fe
Hoang Ninh <ninhhaihoang@gmail.com>
2020-05-20 22:21:20 +0700

    Initial commit

---
+++ demo/src/main/java/example/demo/controller/PostController.java
@@ -0,0 +1,16 @@
+package example.demo.controller;
+import com.example.demo.model.Post;
+import com.example.demo.service.PostService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class PostController {
+}
