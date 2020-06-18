9e128005ff32e65109c5fa7e82e1d3e1662a72a2
nvcgooner <nguyenvancuong18t@gmail.com>
2020-06-18 14:50:32 +0700

    DeleteMaapping

--- doctorfinder-api/src/main/java/com/leoaslan/docfind/controller/DeleteAppointment.java
+++ doctorfinder-api/src/main/java/com/leoaslan/docfind/controller/DeleteAppointment.java
@@ -5,6 +5,7 @@ import com.leoaslan.docfind.service.deleteAppointment.DeleteAppointmentService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class DeleteAppointment {
     @Autowired
     DeleteAppointmentService deleteAppointmentService;
 
-    @GetMapping("/delete")
+    @DeleteMapping("/delete")
     ResponseEntity<?> deleteAppointment(HttpServletRequest request){
         int appointmentID = Integer.parseInt(request.getParameter("appointmentID"));
         log.info(appointmentID);
